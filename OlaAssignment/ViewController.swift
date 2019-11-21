09a7089eb8fa7d30952af4e3ba7f98a0ed765847
Himanshu Saraswat <himanshu.saraswat@yatra.com>
2019-11-21 18:17:03 +0530

    Initial Commit

---
+++ OlaAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OlaAssignment
+//
+//  Created by Himanshu Saraswat on 21/11/19.
+//  Copyright © 2019 Himanshu Saraswat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
